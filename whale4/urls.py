0d106361a01c68759934fde711ed7e2bf953a703
Sylvain Bouveret <sylvain.bouveret@imag.fr>
2016-05-02 07:55:16 +0200

    Login / Logout pages
    - Login and logout templates are now part of the accounts application.
    - Logout template has been added.

--- whale4/urls.py
+++ whale4/urls.py
@@ -13,7 +13,7 @@ urlpatterns = patterns('',
                        url(r'^admin-poll$', 'whale4.views.admin_poll', name='administrate poll'),
                        url(r'^vote$', 'whale4.views.vote', name='vote'),
                        
-                       url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
-                       url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
+                       url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'accounts/login.html'}, name='login'),
+                       url(r'^logout/$', 'django.contrib.auth.views.logout', {'template_name': 'accounts/logout.html'}, name='logout'),
                        url(r'^register/$', RegistrationView.as_view(), name='register'),
 )
