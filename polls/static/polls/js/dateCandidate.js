e7bfad23d580cbff4a4c04a123c2451f914c980a
natetem <mariejeanne.natete@gmail.com>
2016-05-31 16:50:18 +0200

    put datePicker in botton orientation

--- polls/static/polls/js/dateCandidate.js
+++ polls/static/polls/js/dateCandidate.js
@@ -1,24 +1,28 @@
- $(document).ready(function() {
-   $('#add_more').click(function() {
-    var form_idx = $('#id_form-TOTAL_FORMS').val();
-    $('#form_set').append($('#empty_form').html().replace(/__prefix__/g, form_idx));
-    $('#id_form-TOTAL_FORMS').val(parseInt(form_idx) + 1);
-  });
+$(document).ready(function() {
+    $('#add_more').click(function() {
+        var form_idx = $('#id_form-TOTAL_FORMS').val();
+        $('#form_set').append($('#empty_form').html().replace(/__prefix__/g, form_idx));
+        $('#id_form-TOTAL_FORMS').val(parseInt(form_idx) + 1);
+    });
 
-  $('.datepicker').datepicker({
-    format: 'yyyy-mm-dd',
-    multidate: true
-  });
 
-  $('.delete').click( function(){ 
-   $(this).remove(); 
- });
+    $('.datepicker').datepicker({
+        format: 'yyyy-mm-dd',
+        multidate: true,
+orientation: "bottom auto",
+    });
 
-     $("#candidate_formset").hide();
+
+    $('.delete').click( function(){
+        $(this).remove();
+    });
+
+
+    $("#candidate_formset").hide();
+
 
     $("#id_dates").click(function(){
-        $("#candidate_formset").show();
-
+        $("#candidate_formset").show("slow");
     });
 
 });
