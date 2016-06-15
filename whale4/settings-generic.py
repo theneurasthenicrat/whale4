a32e2d50f2960eee59f0baf05b1528419c5ac082
natetem <mariejeanne.natete@gmail.com>
2016-06-15 17:37:43 +0200

    some change for settings

--- whale4/settings-generic.py
+++ whale4/settings-generic.py
@@ -139,8 +139,8 @@ BOOTSTRAP3 = {
     'horizontal_field_class': 'col-md-8',}
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'mariejeanne.natete'
-EMAIL_HOST_PASSWORD = 'password'
+EMAIL_HOST_USER = 'whale4.ad'
+EMAIL_HOST_PASSWORD = 'whale4ad'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
