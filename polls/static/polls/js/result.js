d76f85e2ab6e75e1e8544816f491b5e75d4e0735
natetem <mariejeanne.natete@gmail.com>
2016-08-11 17:05:06 +0200

    add graph of run off method

--- polls/static/polls/js/result.js
+++ polls/static/polls/js/result.js
@@ -53,6 +53,12 @@ function graph() {
 
                 break;
 
+            case 3:
+                var data = data.condorcet;
+                runoff_plot(data);
+
+                break;
+
             default:
 
         }
@@ -142,53 +148,59 @@ function condorcet_plot(data) {
     var nodes = data.nodes,
         links = data.links,
         colorTab = ["red", "#e1dd38", "green"],
-        width = window.innerWidth/4,
-        height = window.innerHeight/2,
-        width1 = window.innerWidth/6,
-        height1 = window.innerWidth/6,
-        minNodeValue = d3.min(nodes, function(d) { return d.value; }),
-        meanNodeValue = d3.mean(nodes, function(d) { return d.value; }),
-        maxNodeValue = d3.max(nodes, function(d) { return d.value; });
+        width = window.innerWidth / 4,
+        height = window.innerHeight / 2,
+        width1 = window.innerWidth / 6,
+        height1 = window.innerWidth / 6,
+        minNodeValue = d3.min(nodes, function (d) {
+            return d.value;
+        }),
+        meanNodeValue = d3.mean(nodes, function (d) {
+            return d.value;
+        }),
+        maxNodeValue = d3.max(nodes, function (d) {
+            return d.value;
+        });
 
     var color = d3.scale.linear()
-        .domain([minNodeValue,meanNodeValue ,maxNodeValue])
+        .domain([minNodeValue, meanNodeValue, maxNodeValue])
         .range(colorTab);
 
     var margin = {top: 60, right: 20, bottom: 20, left: 60};
 
 
-    var maxLinkValue = d3.max(links, function(d) { return d.value; });
+    var maxLinkValue = d3.max(links, function (d) {
+        return d.value;
+    });
 
 
     var x = d3.scale.ordinal().rangeBands([0, width1]),
         z = d3.scale.linear().domain([0, 4]).clamp(true);
 
 
-
-
     var color1 = d3.scale.linear()
-        .domain([-maxLinkValue,0,maxLinkValue])
+        .domain([-maxLinkValue, 0, maxLinkValue])
         .range(["red", "#e1dd38", "green"]);
 
     var matrix = [],
         n = nodes.length;
 
 
-
     var i;
 
-    for (i = 0; i < nodes.length;i++) {
-        matrix[i] = d3.range(n).map(function(j) { return {x: j, y: i, z: 0}; });
+    for (i = 0; i < nodes.length; i++) {
+        matrix[i] = d3.range(n).map(function (j) {
+            return {x: j, y: i, z: 0};
+        });
     }
 
 
     // Convert links to matrix; count character occurrences.
-    links.forEach(function(link) {
+    links.forEach(function (link) {
         matrix[link.source][link.target].z = link.value;
-        matrix[link.target][link.source].z = 0-link.value;
+        matrix[link.target][link.source].z = 0 - link.value;
 
     });
-
 
 
     // The default sort order.
@@ -198,11 +210,11 @@ function condorcet_plot(data) {
         .nodes(d3.values(nodes))
         .links(links)
         .size([width, height])
-        .linkDistance(width/1.5)
+        .linkDistance(width / 1.5)
         .charge(-50)
         .on("tick", tick)
         .start();
- d3.selectAll("svg").remove();
+    d3.selectAll("svg").remove();
     var svg = d3.select("#graph").append("svg")
         .attr("width", width)
         .attr("height", height);
@@ -217,7 +229,7 @@ function condorcet_plot(data) {
         .attr("markerWidth", 10)
         .attr("markerHeight", 10)
         .attr("orient", "auto")
-        .style("fill","#ccc")
+        .style("fill", "#ccc")
         .append("svg:path")
         .attr("d", "M 0 0 L 10 5 L 0 10 z");
 
@@ -236,19 +248,25 @@ function condorcet_plot(data) {
         .call(force.drag);
 
     node.append("circle")
-        .attr("r",16)
-        .style("fill", function(d) { return color(d.value); })
-        .style("stroke","#fff")
-        .style("stroke-width",1.5);
+        .attr("r", 16)
+        .style("fill", function (d) {
+            return color(d.value);
+        })
+        .style("stroke", "#fff")
+        .style("stroke-width", 1.5);
 
     node.append("text")
         .attr("x", 22)
         .attr("dy", ".35em")
-        .style("fill", function(d) { return color(d.value); })
-        .text(function(d) { return d.name; });
+        .style("fill", function (d) {
+            return color(d.value);
+        })
+        .text(function (d) {
+            return d.name;
+        });
 
     function tick() {
-        path.attr("d", function(d) {
+        path.attr("d", function (d) {
             var dx = d.target.x - d.source.x,
                 dy = d.target.y - d.source.y;
 
@@ -260,8 +278,9 @@ function condorcet_plot(data) {
         });
 
         node
-            .attr("transform", function(d) {
-                return "translate(" + d.x + "," + d.y + ")"; });
+            .attr("transform", function (d) {
+                return "translate(" + d.x + "," + d.y + ")";
+            });
     }
 
     var matrixSvg = d3.select("#graph").append("svg")
@@ -273,8 +292,6 @@ function condorcet_plot(data) {
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 
-
-
     matrixSvg.append("rect")
         .attr("width", width1)
         .attr("height", height1);
@@ -283,50 +300,155 @@ function condorcet_plot(data) {
         .data(matrix)
         .enter().append("g")
         .attr("class", "row")
-        .attr("transform", function(d, i) {  return "translate(0," + x(i) + ")"; })
+        .attr("transform", function (d, i) {
+            return "translate(0," + x(i) + ")";
+        })
         .each(row);
 
     row.append("line")
         .attr("x2", width1)
-        .style("stroke","#fff");
+        .style("stroke", "#fff");
 
     row.append("text")
         .attr("x", -6)
         .attr("y", x.rangeBand() / 5)
         .attr("dy", ".32em")
         .attr("text-anchor", "end")
-        .style("fill", function(d,i) { return color(nodes[i].value); })
-        .text(function(d, i) { return nodes[i].name; });
+        .style("fill", function (d, i) {
+            return color(nodes[i].value);
+        })
+        .text(function (d, i) {
+            return nodes[i].name;
+        });
 
     var column = matrixSvg.selectAll(".column")
         .data(matrix)
         .enter().append("g")
         .attr("class", "column")
-        .attr("transform", function(d, i) { return "translate(" + x(i) + ")rotate(-90)"; });
+        .attr("transform", function (d, i) {
+            return "translate(" + x(i) + ")rotate(-90)";
+        });
 
     column.append("line")
         .attr("x1", -width1)
-        .style("stroke","#fff");
+        .style("stroke", "#fff");
 
     column.append("text")
         .attr("x", 6)
         .attr("y", x.rangeBand() / 5)
         .attr("dy", ".32em")
         .attr("text-anchor", "start")
-        .style("fill", function(d,i) { return color(nodes[i].value); })
-        .text(function(d, i) { return nodes[i].name; });
+        .style("fill", function (d, i) {
+            return color(nodes[i].value);
+        })
+        .text(function (d, i) {
+            return nodes[i].name;
+        });
 
     function row(row) {
         var cell = d3.select(this).selectAll(".cell")
             .data(row)
             .enter().append("rect")
             .attr("class", "cell")
-            .attr("x", function(d) { return x(d.x); })
+            .attr("x", function (d) {
+                return x(d.x);
+            })
             .attr("width", x.rangeBand())
             .attr("height", x.rangeBand())
-            .style("fill", function(d) { return d.z==0 ?"#ccc" :color1(d.z ); });
+            .style("fill", function (d) {
+                return d.z == 0 ? "#ccc" : color1(d.z);
+            });
 
     }
 
+}
+
+function runoff_plot(data) {
+    var colorTab = [ "green","#e1dd38","red"],
+        width = window.innerWidth / 2,
+        height = window.innerHeight / 2;
+
+  var matrix = [["a","b","c","d"],["a","b","c"], ["a","b"],["a"]];
+    var color = d3.scale.linear()
+        .domain([0,0.5,1])
+        .range(colorTab);
+
+    var margin = {top: 20, right: 20, bottom: 20, left: 20};
+
+
+    var x = d3.scale.ordinal().rangeBands([0, width]);
+    x.domain(d3.range(matrix.length));
+
+    var y = d3.scale.ordinal().rangeBands([0, height-170]);
+    y.domain(d3.range(matrix[0].length));
+
+
+    d3.selectAll("svg").remove();
+
+    var matrixSvg = d3.select("#graph").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .style("margin-left", margin.left + "px")
+        .style("margin-top", margin.top + "px")
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var row = matrixSvg.selectAll(".round")
+        .data(matrix);
+
+    var roundG = row.enter()
+        .append("g")
+        .attr("class", "round")
+        .attr("id", function(d, i) {return "round" + i;});
+
+    roundG
+        .append("rect")
+        .attr("y", 0)
+        .attr("width",x.rangeBand()-10)
+        .attr("height", height)
+        .attr("fill", "yellow")
+        .attr("opacity", 0.1);
+
+    roundG
+        .append("text")
+        .attr("x", 0)
+        .attr("dy", ".35em")
+        .attr("text-anchor", "middle")
+        .text(function(d, i) {return i == (matrix.length-1) ? "Winner" : "Round " + i;})
+        .attr("transform", function(d, j) {return "translate(" + (y.rangeBand()/ 2) + "," + (50 / 2) + ")"; });
+
+    var roundElmt = row.selectAll(".round")
+        .data(function(d) {return d;});
+
+    row
+        .attr("transform", function(d, i) {return "translate(" + ( x.rangeBand() * i)  + ")"; });
+
+    var insideG = roundElmt.enter()
+        .append("g")
+        .attr("class", function(d, i) {return "g" + i;});
+    var n= matrix[0].length;
+
+    roundElmt
+        .attr("transform", function(d, j) {return "translate(" + 0 + "," + (((height-100)/n * j )+100)+ ")"; });
+
+    insideG
+        .append("rect")
+        .attr("x", "10px")
+        .attr("y", "-1em")
+        .attr("width", x.rangeBand()-30)
+        .attr("height", y.rangeBand())
+        .style("fill",function(d, i,j) {return matrix[j].length==1?color(i): color((i)/(matrix[j].length-1));})
+        .attr("opacity", 0.8);
+
+    insideG
+        .append("text")
+        .attr("fill", "#fff")
+        .attr("font-weight", "bold")
+        .style("text-anchor", "middle")
+        .attr("x", (x.rangeBand())/6)
+        .attr("y",(y.rangeBand())/6)
+        .attr("dy", ".35em")
+        .text(String);
 
 }
+
