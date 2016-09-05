c6bed82b658cd8309a48083f53ea4566d49c6652
Sylvain Bouveret <sylvain.bouveret@imag.fr>
2016-09-05 21:26:11 +0200

    Removed unused import patterns in urls.py
    This import could make the application throw an exception when run with django version >= 1.10.

--- polls/urls.py
+++ polls/urls.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from polls import views
 
 uuid4="[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12}"
